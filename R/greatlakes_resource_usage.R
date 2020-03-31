8eb331237128aa0f8f959ae00a9b0e6715a378fd
Katie Saund <katiephd@umich.edu>
2020-03-31 10:42:04 -0400

    Update documentation for demo

--- R/greatlakes_resource_usage.R
+++ R/greatlakes_resource_usage.R
@@ -3,29 +3,29 @@
 # 2020/01/29
 # Ryan D. Crawford
 # ------------------------------------------------------------------------------
-# Take a job id and return the Job Wall-clock time in hours and the Memory 
+# Take a job id and return the Job Wall-clock time in hours and the Memory
 # Utilized in GB as a numeric vector
 # ------------------------------------------------------------------------------
 
-# 2020/03/24 Katie Saund added two catches for edge cases: 
+# 2020/03/24 Katie Saund added two catches for edge cases:
 # (1) When the memory usage is basically 0MB
 # (2) When the run time is more than 1 day
 
 #' Get memory usage and run time for a Great Lakes sbatch job given the job id
-#' 
-#' @description This function needs to be run from the command line, not within 
+#'
+#' @description This function needs to be run from the command line, not within
 #'   an Rstudio session due to a call of system(). T
 #'
 #' @param jobId Character or numeric
 #'
-#' @return Numeric Vector. Memory in GB and time in hours. 
+#' @return Numeric Vector. Memory in GB and time in hours.
 #' @export
 GetResourceUsage = function( jobId )
 {
   # Run the system command to get the seff result for this job ID
   seff = system( paste("seff", jobId ), intern = TRUE)
-  
-  # Find the position of the time and the memory utilized in the 
+
+  # Find the position of the time and the memory utilized in the
   # seff result
   wallClockStr = "Job Wall-clock time: "
   wallTime = gsub(wallClockStr, '', seff[ grep(wallClockStr, seff) ])
@@ -37,7 +37,7 @@ GetResourceUsage = function( jobId )
   if (grepl("(estimated maximum)", memoryUsage)) {
     memoryUsage <- "0.00 MB"
   }
-  
+
   # If memory usage from seff if returned in MB, convert to GB
   if ( grepl("MB", memoryUsage) )
   {
@@ -50,23 +50,26 @@ GetResourceUsage = function( jobId )
     memoryUsage = gsub("KB", '', memoryUsage)
     memoryUsage = as.numeric( trimws(memoryUsage) ) * 1000
   }
-  
+
   return( c(ConvertTimeToHours(wallTime), memoryUsage) )
 }
 
+
+
+
 #' Convert sbatch job run time into hours
-#' 
-#' @description the `$ seff <jobid>` command on Great Lakes returns a string 
-#'   that includes the run time of an sbatch job. These strings look like: 
-#'   * Job Wall-clock time: 10:30:00 
-#'   or 
+#'
+#' @description the `$ seff <jobid>` command on Great Lakes returns a string
+#'   that includes the run time of an sbatch job. These strings look like:
+#'   * Job Wall-clock time: 10:30:00
+#'   or
 #'   * Job Wall-clock time: 1-10:30:00
-#'   and indicate 10.5 hours or 34.5 hours, respectively. Given such a string 
-#'   this function converts the number portion into just hours. The time is 
+#'   and indicate 10.5 hours or 34.5 hours, respectively. Given such a string
+#'   this function converts the number portion into just hours. The time is
 #'   recorded as hours:minutes:seconds.
-#'   
+#'
 #' @param timeStr String of the form: 1-10:30:00 or 10:30:00. The number before
-#'   the dash indicates days. 
+#'   the dash indicates days.
 #'
 #' @return Number of hours rounded to 3 digits. Numeric.
 #' @export
@@ -83,21 +86,21 @@ ConvertTimeToHours = function(timeStr)
   if (!grepl("[0-9][0-9][:][0-9][0-9][:][0-9][0-9]", timeStr)) {
     stop("Input must be of the format 10:00:00 or 1-10:00:00")
   }
-  
+
   # Function -------------------------------------------------------------------
   days <- 0
-  # When there are 1 or more days of run time 
+  # When there are 1 or more days of run time
   if (grepl("-", timeStr)) {
     days <- as.numeric(unlist(strsplit(timeStr, "-"))[1])
   }
   days_in_hours <- days * 24
-  
+
   # Time ignoring days
   timeStr_without_days <- gsub(".*[-]", "", timeStr)
   times <- as.numeric( unlist(strsplit(timeStr_without_days, ':') ) )
   expVals = c(0, 1, 2)
   hours   = sum( sapply( seq(3), function(i) times[i] / 60**expVals[i] ) )
-  
+
   # Add hours from days plus hours without days together
   total <- sum(hours, days_in_hours)
   return(round(total, 3))
