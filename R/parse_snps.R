3ab5fad5e3bce5c9b8c417b6c6866b84542930f0
Zena Lapp <zenalapp@gmail.com>
2020-12-07 14:28:16 -0500

    Fix bug

--- R/parse_snps.R
+++ R/parse_snps.R
@@ -269,7 +269,7 @@ parse_snps <- function(varmat_code,
   split_rows_flag <- varmat_code_split_list[[4]]
 
   if (return_binary_matrix) {
-    alleles <- alleles[split_rows_flag, ]
+    alleles <- alleles[split_rows_flag]
   }
 
   major_alleles <- major_alleles[split_rows_flag]
