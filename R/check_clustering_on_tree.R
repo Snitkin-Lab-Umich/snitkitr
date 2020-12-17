4540475ae563a256720b74af1f03276c2e9469d2
Zena Lapp <zenalapp@gmail.com>
2020-12-17 06:46:33 -0500

    Add option to modify bootstrap support threshold

--- R/check_clustering_on_tree.R
+++ R/check_clustering_on_tree.R
@@ -22,12 +22,13 @@ reverse_list_str <- function(ls) { # @Josh O'Brien
 #' @param subtrs Subtrees created using ape::subtrees to look for clustering on. Should include all isolates of interest.
 #' @param isolate_labels Named vector of labels by which pure clusters are defined. Names must be equivalent to tree tip label names.
 #' @param control_labels Named vector of labels known to cluster. Names must be equivalent to tree tip label names. This controls for clustering by requiring that the pure clusters must contain multiple of the control labels. 
+#' @param bootstrap Bootstrap support to use to filter unconfident tree edges (keeps > bootstrap; NULL = keep all; default: 90).
 #'
-#' @return list containing the largest pure subtree that each isolate belongs to and the index of that subtree
+#' @return list containing the largest pure subtree that each isolate belongs to, the index of that subtree, and the edges in that subtree.
 #' @export
 #'
 #' @examples
-get_largest_subtree <- function(subtrs, isolate_labels, control_labels=NULL){
+get_largest_subtree <- function(subtrs, isolate_labels, control_labels=NULL, bootstrap = 90){
   largest_st_info = future.apply::future_lapply(names(isolate_labels), function(i){
     #DETERMINE THE LARGEST CLUSTER WHICH EACH ISOLATE BELONGS TO. 
     #CLUSTERS ARE DEFINED AS:
@@ -35,7 +36,10 @@ get_largest_subtree <- function(subtrs, isolate_labels, control_labels=NULL){
     sts = future.apply::future_sapply(subtrs, FUN = function(st){ 
       i_in_subtree = sum(grepl(i, st$tip.label, perl = TRUE)) > 0 # isolate is in subtree
       one_label = length(unique(isolate_labels[intersect(st$tip.label, names(isolate_labels))])) == 1 # only one label in subtree
-      good_bootstrap = !is.na(as.numeric(st$node.label[[1]])) && as.numeric(st$node.label[[1]]) > 90 # bootstrap support > 90
+      good_boostrap = rep(TRUE, length(st$node.label[[1]]))
+      if(!is.null(bootstrap)){
+        good_bootstrap = !is.na(as.numeric(st$node.label[[1]])) && as.numeric(st$node.label[[1]]) > bootstrap # bootstrap support > 90
+      }
       multiple_control = ifelse(is.null(control_labels), TRUE, # always true if not controlling for another variable
                                 length(unique(control_labels[intersect(st$tip.label, names(control_labels))])) > 1) # more than one control label in subtree
       if(i_in_subtree && one_label && good_bootstrap && multiple_control){ 
