c385fe74b4718bc61e9494789d96e05b3d9f3b06
Katie Saund <katiephd@umich.edu>
2021-01-04 11:19:21 -0500

    Save small version of cdif SNP datasets to use for unit testing

--- R/temp_save_small_data_sets.R
+++ R/temp_save_small_data_sets.R
@@ -16,19 +16,22 @@ allele_mat <- read.table("data/SNP_matrix_allele.tsv",
                          quote = "",
                          row.names = 1)
 
-nrow(code_mat)
-nrow(allele_mat)
+nrow(code_mat) # 247564
+nrow(allele_mat) # 247564
 
-# Make dataset a lot smaller by making it 1/10th the size in each dimension.
-rows_to_keep <- seq(1, nrow(code_mat), nrow(code_mat) / 10)
-cols_to_keep <- seq(1, ncol(code_mat), ncol(code_mat / 10))
+# Make dataset a lot smaller by making it 10000 rows and 25 columnes
+rows_to_keep <- floor(seq(1, nrow(code_mat), nrow(code_mat) / 10000))
+cols_to_keep <- floor(seq(1, ncol(code_mat), ncol(code_mat) / 25))
 
-cdif_snp_code_mat <- cdif_code_mat[rows_to_keep, cols_to_keep]
+cdif_snp_code_mat <- code_mat[rows_to_keep, cols_to_keep]
 cdif_snp_allele_mat <- allele_mat[rows_to_keep, cols_to_keep]
 
-identical(ncol(cdif_snp_code_mat), ncol(cdif_snp_allele_mat))
-identical(colnames(cdif_snp_code_mat), colnames(cdif_snp_allele_mat))
-identical(nrow(cdif_snp_code_mat), nrow(cdif_snp_allele_mat))
+dim(cdif_snp_code_mat) # 10000 25
+dim(cdif_snp_allele_mat) # 10000 25
+
+identical(ncol(cdif_snp_code_mat), ncol(cdif_snp_allele_mat)) # TRUE
+identical(colnames(cdif_snp_code_mat), colnames(cdif_snp_allele_mat)) # TRUE
+identical(nrow(cdif_snp_code_mat), nrow(cdif_snp_allele_mat)) # TRUE
 
 save(cdif_snp_code_mat, file = "data/cdif_snp_code_matrix.RData")
 save(cdif_snp_allele_mat, file = "data/cdif_snp_allele_matrix.RData")
