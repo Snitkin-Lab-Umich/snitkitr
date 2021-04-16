b21706f1ea80e2f502c593a4821e41115b6d47d1
Katie Saund <katiephd@umich.edu>
2021-04-16 15:27:54 -0400

    Adjust indexing for allele matrix because dimensions of allele and binary matrices are not the same

--- R/test_referencing_lib.R
+++ R/test_referencing_lib.R
@@ -22,10 +22,15 @@ check_anc_rerefencing <- function(parsed_indel, row) {
 
 check_maj_rerefencing <- function(parsed_indel, row, col_index) {
   # grab the major allele and then check that in the bin mat the positions that correspond to that allele are 0s
-  most_common_occurance <- unname(unlist(parsed_indel$allele$mat[row, ])) %>% table(.) %>% unname() %>% max()
-  most_common_occurance_tbl_log <-  unname(unlist(parsed_indel$allele$mat[row, ])) %>% table() == most_common_occurance
+
+  bin_variant_name <- row.names(parsed_indel$bin$mat)[row]
+  allele_mat_equiv_row <- which(row.names(parsed_indel$allele$mat) == bin_variant_name)
+
+
+  most_common_occurance <- unname(unlist(parsed_indel$allele$mat[allele_mat_equiv_row, ])) %>% table(.) %>% unname() %>% max()
+  most_common_occurance_tbl_log <-  unname(unlist(parsed_indel$allele$mat[allele_mat_equiv_row, ])) %>% table() == most_common_occurance
   maj <- names(most_common_occurance_tbl_log)[most_common_occurance_tbl_log == TRUE]
-  maj_index <- which(unname(unlist(parsed_indel$allele$mat[row, ])) == maj)
+  maj_index <- which(unname(unlist(parsed_indel$allele$mat[allele_mat_equiv_row, ])) == maj)
   min_index <- c(col_index)[!c(col_index) %in% maj_index]
   expect_identical(unname(unlist(parsed_indel$bin$mat[row, ]))[maj_index], rep(0, length(maj_index)))
   expect_identical(unname(unlist(parsed_indel$bin$mat[row, ]))[min_index], rep(1, length(min_index)))
@@ -33,8 +38,13 @@ check_maj_rerefencing <- function(parsed_indel, row, col_index) {
 
 check_match_ref_genome <- function(parsed_indel, row) {
   # grab the ref allele and then check that in the bin mat the positions that correspond to that allele are 0s
+
+  bin_variant_name <- row.names(parsed_indel$bin$mat)[row]
+  allele_mat_equiv_row <- which(row.names(parsed_indel$allele$mat) == bin_variant_name)
+
+
   ref_allele <- unname(unlist(parsed_indel$bin$annots$ref))[row]
-  ref_index <- which(unname(unlist(parsed_indel$allele$mat[row, ])) == ref_allele)
+  ref_index <- which(unname(unlist(parsed_indel$allele$mat[allele_mat_equiv_row, ])) == ref_allele)
   not_ref_index <- c(1:nrow(parsed_indel$bin$mat))[!c(1:nrow(parsed_indel$bin$mat)) %in% ref_index]
   expect_identical(unname(unlist(parsed_indel$bin$mat[row, ]))[ref_index], rep(0, length(ref_index)))
   expect_identical(unname(unlist(parsed_indel$bin$mat[row, ]))[not_ref_index], rep(1, length(not_ref_index)))
